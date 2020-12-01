d3f85d459c10d38efb42975c8aef45a710379a61
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-12-01 16:28:08 +0700

    Revert "Change BASE_DIR"
    This reverts commit 42c1b1783ce3e96eb8a6c4593a878b34a6c3df37.

--- newyear/settings.py
+++ newyear/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +26,7 @@ SECRET_KEY = 'ea^uyte5tc*naw&(ylw)bnb2v1sxg4mz)$u43)=es85^+14k4c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
